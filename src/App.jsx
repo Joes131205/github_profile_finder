569120d070b7144a2bf18f07c36007929f54be94
Joe Steven <stevenjoe1312@gmail.com>
2024-03-02 13:11:10 +0700

    Add search functionality and user state handling
    Introduce a new SearchBar component to allow users to search for profiles and modify the user state in the App component accordingly. This enables displaying user-specific information across different routes. Improves user interaction and data retrieval. No related issues.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,56 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
+import SearchBar from "./SearchBar.jsx";
 
 import Home from "./routes/Home.jsx";
 import Repositories from "./routes/Repositories.jsx";
 import Followers from "./routes/Followers.jsx";
 import Following from "./routes/Following.jsx";
 import Gists from "./routes/Gists.jsx";
+import { useState } from "react";
 
 function App() {
+    const [user, setUser] = useState({});
+
+    function handleUserUpdate(newUser) {
+        setUser(newUser);
+    }
     return (
         <BrowserRouter>
             <Navbar />
+            <SearchBar onUserUpdate={handleUserUpdate} />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/repositories" element={<Repositories />} />
-                <Route path="/followers" element={<Followers />} />
-                <Route path="/following" element={<Following />} />
-                <Route path="/gists" element={<Gists />} />
+                <Route
+                    path="/"
+                    element={
+                        <Home
+                            avatar={user.avatar_url}
+                            bio={user.bio}
+                            blog={user.blog}
+                            username={user.login}
+                            name={user.name}
+                            followers={user.followers}
+                            following={user.following}
+                            createdAt={user.created_at}
+                        />
+                    }
+                />
+                <Route
+                    path="/repositories"
+                    element={<Repositories repositories={user.repos_url} />}
+                />
+                <Route
+                    path="/followers"
+                    element={<Followers followers_url={user.followers_url} />}
+                />
+                <Route
+                    path="/following"
+                    element={<Following following_url={user.following_url} />}
+                />
+                <Route
+                    path="/gists"
+                    element={<Gists gists_url={user.gists_url} />}
+                />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/SearchBar.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function SearchBar({ onUserUpdate }) {
+    const [userInput, setUserInput] = useState("");
+    function handleChange(e) {
+        const name = e.target.value;
+        console.log(name);
+        setUserInput(name);
+    }
+    async function searchUser(e) {
+        e.preventDefault();
+        const response = await fetch(
+            `https://api.github.com/users/${userInput}`
+        );
+        const data = await response.json();
+        const obj = {
+            avatar_url: data.avatar_url,
+            bio: data.bio,
+            blog: data.blog,
+            created_at: data.created_at,
+            followers: data.followers,
+            followers_url: data.followers_url,
+            following: data.following,
+            following_url: data.following_url,
+            html_url: data.html_url,
+            location: data.location,
+            login: data.login,
+            name: data.name,
+            repos_url: data.repos_url,
+            starred_url: `https://api.github.com/users/${userInput}/starred`,
+            gists_url: `https://api.github.com/users/${userInput}/gists`,
+        };
+        onUserUpdate(obj);
+    }
+
+    return (
+        <form onSubmit={searchUser}>
+            <input
+                type="text"
+                placeholder="Github Username..."
+                onChange={handleChange}
+                name="username"
+                id="username"
+            />
+            <input type="submit" value="Search" />
+        </form>
+    );
+}
+
+export default SearchBar;
